6988363a2adf02a130b35271f8ed2043170e7e4a
renatorussobr <renrusso@gmail.com>
2022-10-25 07:09:34 -0400

    Created Google Spreadsheet with raw data

--- joao_8_32_main.R
+++ joao_8_32_main.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(googlesheets4)
 googlesheets4::gs4_deauth()
 googlesheets4::gs4_auth()
+# Searching:
 tweet_mar_aug_2020<-get_all_tweets(query = "João 8 32",
                                    start_tweets = "2022-09-25T00:00:00Z",
                                    end_tweets = "2022-10-08T00:00:00Z", 
@@ -10,11 +11,19 @@ tweet_mar_aug_2020<-get_all_tweets(query = "João 8 32",
                                    data_path = "data",
                                    n = 10000,
                                    is_retweet = FALSE)
-View(tweets_joao_8_32)
+
+# Changing the name, because I originally forgot to do it:
 tweets_joao_8_32 <- tweet_mar_aug_2020
+
+# Saving a Rda file:
 save(tweets_joao_8_32, file = 'joao_8_32_1_turno.rda')
 
-tweets_joao_8_32_txt <- tweets_joao_8_32 |> 
-  select(conversation_id:created_at)
+# Creating a spreadsheet to be stored in google drive:
+tweets_joao_8_32_txt <- tweets_joao_8_32 |>
+  select(conversation_id:created_at, in_reply_to_user_id)
 
-write_sheet(tweets_joao_8_32_txt)
+write_sheet(tweets_joao_8_32_txt,
+            ss = "https://docs.google.com/spreadsheets/d/1zacPKjrm906FaKybf9mPOtDEklCbcYLrEdvv1Ou1JEA")
+
+colSums(is.na(tweets_joao_8_32))
+# number of tweets that are reply to another user: 4713 - 1118 = 3595
